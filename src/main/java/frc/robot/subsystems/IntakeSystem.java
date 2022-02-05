076a37c4069dbe2a76e48a044ca9019e557dfa35
Driver Station <49418132+DriverStation@users.noreply.github.com>
2022-02-05 10:48:14 -0600

    Added Intake Functionalities

--- src/main/java/frc/robot/subsystems/IntakeSystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSystem.java
@@ -1,4 +1,5 @@
 package frc.robot.subsystems;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.revrobotics.CANSparkMax;
@@ -9,32 +10,57 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import frc.robot.Constants.IntakeConstants;
 
 public class IntakeSystem extends SubsystemBase {
-    //the intake motor controller
-    final CANSparkMax intakeController = new CANSparkMax(IntakeConstants.INTAKE_CONTROLLER_PORT, MotorType.kBrushless);
+    //the intake motor controller for running and for deploying
+    final CANSparkMax intakeRunController = new CANSparkMax(IntakeConstants.INTAKE_RUN_PORT, MotorType.kBrushless);
+    final CANSparkMax intakeDeployController = new CANSparkMax(IntakeConstants.INTAKE_DEPLOY_PORT, MotorType.kBrushless);
 
-    //the intake motor controller encoder
-    final RelativeEncoder intakeControllerEncoder = intakeController.getEncoder();
+    //the intake motor controller encoders
+    final RelativeEncoder intakeRunControllerEncoder = intakeRunController.getEncoder();
+    final RelativeEncoder intakeDeployControllerEncoder = intakeDeployController.getEncoder();
 
     //Creates the IntakeSubsystem
     public IntakeSystem() {
 
         //set to factory default and idle so we know what we're working with 
-        intakeController.restoreFactoryDefaults(); 
-        intakeController.setIdleMode(IdleMode.kCoast);
+        intakeRunController.restoreFactoryDefaults();
+        intakeRunController.setIdleMode(IdleMode.kCoast);
+
+        intakeDeployController.restoreFactoryDefaults();
+        intakeDeployController.setIdleMode(IdleMode.kBrake);
 
     }
 
-    public void IntakeRun(double speed) {
-        intakeController.set(speed);
+    public void IntakeRunnerRun(double speed) {
+        intakeRunController.set(speed);
     }
 
-    public void IntakeReverse(double speed) {
-        intakeController.set(-speed);
+    public void IntakeRunnerReverse(double speed) {
+        intakeRunController.set(-speed);
     }
 
-    public void IntakeStop() {
-        intakeController.stopMotor();
+    public void IntakeRunnerStop() {
+        intakeRunController.stopMotor();
     }
 
+    public void IntakeDeployerRunnerUp(double speed) {
+        intakeDeployController.set(speed);
+    }
 
+    public void IntakeDeployerRunnerDown(double speed) {
+        intakeDeployController.set(-speed);
+    }
+
+    public void IntakeDeployerRunnerSoftDrop(double time) {
+        Timer timer = new Timer();
+        timer.start();
+        while (timer.get() < time) {
+            intakeDeployController.set(.15);
+        }
+        intakeDeployController.stopMotor();
+        timer.stop();
+    }
+
+    public void IntakeDeployerRunnerStop() {
+        intakeDeployController.stopMotor();
+    }
 }
