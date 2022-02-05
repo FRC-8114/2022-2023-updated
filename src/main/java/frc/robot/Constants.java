cf6b682c43bdf19b34159c0d7b0952cf50f06db0
jack.g.holt@gmail.com <jack.g.holt@gmail.com>
2022-02-05 10:38:55 -0600

    Added shooter constants.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,6 +12,16 @@ public final class Constants {
     public static final boolean LEFT_MOTORS_INVERSED = false;
   }
 
+  public static final class ShooterConstants {
+    public static final double SHOOTER_DISTANCE_PER_PULSE = 0;
+    public static final double VELOCITY_CONVERSION_FACTOR = 0;
+    public static final double ENCODER_DISTANCE_PER_PULSE = 0;
+    public static double MAX_INPUT = 0;
+    public static final int LEFT_SHOOTER_CONTROLLER_PORT = 0;
+    public static final int RIGHT_SHOOTER_CONTROLLER_PORT = 0;
+    public static final int KICKER_CONTROLLER_PORT = 0;
+  }
+
   public static final class IntakeConstants {
     public static final int INTAKE_RUN_PORT = 5;
     public static final int INTAKE_DEPLOY_PORT = 6;
