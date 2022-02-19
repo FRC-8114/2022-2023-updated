4ea77b20cc55a201c3a8de1fbd30a3cded2879bc
jack.g.holt@gmail.com <jack.g.holt@gmail.com>
2022-02-19 13:52:42 -0600

    Solves unresolved merge conflict.

--- src/main/java/frc/robot/subsystems/FieldPositioningSystem.java
+++ src/main/java/frc/robot/subsystems/FieldPositioningSystem.java
@@ -30,7 +30,10 @@ public class FieldPositioningSystem extends SubsystemBase {
     public void initializePosition(double[] position, double angle) {
         navx = new AHRS(SerialPort.Port.kUSB);
         navx.calibrate();
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
         this.position = position;
         this.angle = navx.getYaw();
     }
