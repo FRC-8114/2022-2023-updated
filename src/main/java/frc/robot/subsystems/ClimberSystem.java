26ac3afdada60bd55dddbf9d313e84e7e37a6094
IsaacSueltenfuss <iasueltenfuss@gvtc.com>
2022-03-03 23:23:27 -0600

    Added Voltage Control to ClimberSystem and Updated Motors

--- src/main/java/frc/robot/subsystems/ClimberSystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSystem.java
@@ -10,18 +10,20 @@ import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 public class ClimberSystem extends SubsystemBase {
-    final TalonFX climberRunController = new TalonFX(ClimberConstants.CLIMBER_RUN_PORT);
+    // Creates the motor controllers for the dart and the arm
+    final WPI_TalonFX climberRunController = new WPI_TalonFX(ClimberConstants.CLIMBER_RUN_PORT);
     final CANSparkMax climberDeployController = new CANSparkMax(ClimberConstants.CLIMBER_DEPLOY_PORT,
             MotorType.kBrushless);
 
+    // Creates a dart encoder
     final RelativeEncoder climberDeployControllerEncoder = climberDeployController.getEncoder();
 
+    // Initializes a potentiometer
     final AnalogPotentiometer potentiometer = new AnalogPotentiometer(0);
-    // Creates the IntakeSubsystem
+
     public ClimberSystem() {
 
         // set to factory default and idle so we know what we're working with
@@ -37,34 +39,62 @@ public class ClimberSystem extends SubsystemBase {
         climberDeployController.stopMotor();
     }
 
-    public void ClimberRunnerUp(double speed) {
-        climberRunController.set(TalonFXControlMode.PercentOutput, speed);
+    // Runs the climber arm at a speed from 0 to 1.0
+    public void ClimberRunnerUp (double speed) {
+        climberRunController.set(speed);
     }
 
-    public void ClimberRunnerDown(double speed) {
-        climberRunController.set(TalonFXControlMode.PercentOutput, -speed);
+    // Runs the climber arm at a given voltage
+    public void ClimberRunnerUpVoltage (double voltage) {
+        climberRunController.setVoltage(voltage);
     }
 
+    // Reverses the climber arm at a speed from 0 to 1.0
+    public void ClimberRunnerDown (double speed) {
+        climberRunController.set(-speed);
+    }
+
+    // Reverses the climber arm at a given voltage
+    public void ClimberRunnerDownVoltage (double voltage) {
+        climberRunController.setVoltage(-voltage);
+    }
+
+    // Stops all motion on the arm
     public void ClimberStop() {
-        climberRunController.set(TalonFXControlMode.PercentOutput, 0);
+        climberRunController.setVoltage(0);
     }
 
-    public void ClimberDeployerUp(double speed) {
+    // Runs the climber dart at a speed from 0 to 1.0
+    public void ClimberDeployerUp (double speed) {
         climberDeployController.set(speed);
     }
 
-    public void ClimberDeployerDown(double speed) {
+    // Runs the climber dart at a given voltage
+    public void ClimberDeployerUpVoltage (double voltage) {
+        climberDeployController.setVoltage(voltage);
+    }
+
+    // Reverses the climber dart at a speed from 0 to 1.0
+    public void ClimberDeployerDown (double speed) {
         climberDeployController.set(-speed);
     }
 
-    public void ClimberDeployerStop() {
-        climberDeployController.stopMotor();
+    // Reverses the climber dart at a given voltage
+    public void ClimberDeployerDownVoltage (double voltage) {
+        climberDeployController.setVoltage(-voltage);
     }
 
+    // Stops all motion on the dart
+    public void ClimberDeployerStop() {
+        climberDeployController.setVoltage(0);
+    }
+
+    // Get the current angle of the potentiometer
     public double getPotentiometerAngle() {
         return potentiometer.get();
     }
 
+    // Get the current extended length of the dart
     public double getDartPosition() {
         return 6 + getPotentiometerAngle() * (6/4.4);
     }
