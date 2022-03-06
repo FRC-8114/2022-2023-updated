eeede2076f704236af1d6e90de88fddded3dda88
DESKTOP-UCHK3QL\programming <crchampie@gmail.com>
2022-03-05 23:20:29 -0600

    Implemented time-based auto shooting instead of on RPM

--- src/main/java/frc/robot/commands/auto/AutoShoot.java
+++ src/main/java/frc/robot/commands/auto/AutoShoot.java
@@ -3,37 +3,30 @@ package frc.robot.commands.auto;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
+import frc.robot.Constants.ShooterConstants;
 import frc.robot.subsystems.ShooterSystem;
 
 public class AutoShoot extends CommandBase {
     private ShooterSystem shooterSystem;
-    private int desired_rpm = 0, count = 0;
+    private int count = 0;
     private Timer timer;
-    private double shootingTime;
 
-    public AutoShoot(int desired_rpm, ShooterSystem shooterSystem) {
-        this.desired_rpm = desired_rpm;
+    public AutoShoot(ShooterSystem shooterSystem) {
         this.shooterSystem = shooterSystem;
 
         timer = new Timer();
     }
 
     public void initialize() {
-        shooterSystem.ShooterRunVoltage((desired_rpm - Constants.RPM_TO_VOLTAGE_CONSTANT)/ Constants.RPM_TO_VOLTAGE);
-
         timer.start();
     }
 
     public void execute() {
-        if (ShooterSystem.ShooterRPM >= desired_rpm) {
+        shooterSystem.ShooterRunVoltage(6.5);
+
+        if (timer.get() > ShooterConstants.SHOOTER_SPIN_UP_TIME) {
             shooterSystem.UpperKickerRun(1);
             shooterSystem.LowerKickerRun(1);
-            if(shootingTime == 0) {
-                shootingTime = timer.get();
-            }
-            if(timer.get() > shootingTime + 1) {
-                count++;
-            }
         }
         else {
             shooterSystem.UpperKickerStop();
@@ -48,7 +41,7 @@ public class AutoShoot extends CommandBase {
     }
 
     public boolean isFinished() {
-        if (count >= 1)
+        if (timer.get() > 4)
             return true;
         return false;
     }
