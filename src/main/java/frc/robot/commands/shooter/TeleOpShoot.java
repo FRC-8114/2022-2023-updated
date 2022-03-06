09a1ca49ab1786c9655b05ce6965dee3605668c3
DESKTOP-UCHK3QL\programming <crchampie@gmail.com>
2022-03-05 23:19:34 -0600

    Implemented time-based shooting instead of RPM

--- src/main/java/frc/robot/commands/shooter/TeleOpShoot.java
+++ src/main/java/frc/robot/commands/shooter/TeleOpShoot.java
@@ -1,26 +1,31 @@
 package frc.robot.commands.shooter;
 
+
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
+import frc.robot.Constants.ShooterConstants;
 import frc.robot.subsystems.ShooterSystem;
 
 public class TeleOpShoot extends CommandBase {
-    private double desiredRPM, lowerKickerSpeed, upperKickerSpeed;
+    private double lowerKickerSpeed, upperKickerSpeed;
     private ShooterSystem shooterSystem;
+    private Timer timer;
 
-    public TeleOpShoot(double desiredRPM, double lowerKickerSpeed, double upperKickerSpeed, ShooterSystem shooterSystem) {
-        this.desiredRPM = desiredRPM;
+    public TeleOpShoot(double lowerKickerSpeed, double upperKickerSpeed, ShooterSystem shooterSystem) {
         this.lowerKickerSpeed = lowerKickerSpeed;
         this.upperKickerSpeed = upperKickerSpeed;
         this.shooterSystem = shooterSystem;
+        timer = new Timer();
 
     }
     public void initialize() {
+        timer.start();
 
     }
     public void execute() {
-        shooterSystem.ShooterRunVoltage((desiredRPM - Constants.RPM_TO_VOLTAGE_CONSTANT)/ Constants.RPM_TO_VOLTAGE);
-        if (shooterSystem.ShooterRPM >= .95 * desiredRPM) {
+        shooterSystem.ShooterRunVoltage(6.5);
+        if (timer.get() > ShooterConstants.SHOOTER_SPIN_UP_TIME) {
             shooterSystem.LowerKickerRun(lowerKickerSpeed);
             shooterSystem.UpperKickerRun(upperKickerSpeed);
 
@@ -29,7 +34,7 @@ public class TeleOpShoot extends CommandBase {
     }
 
     public void end() {
-
+        timer.stop();
     }
 
     public boolean isFinished() {
