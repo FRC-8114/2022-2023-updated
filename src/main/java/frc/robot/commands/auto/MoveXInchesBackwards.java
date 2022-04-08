f418bd62eded3f9a805b8c241e6eb419445f9923
DESKTOP-7Q39R8S\Kathy Champie <crchampie@gmail.com>
2022-04-08 18:43:42 -0500

    Ramp Rate Changes in MoveXInches

--- src/main/java/frc/robot/commands/auto/MoveXInchesBackwards.java
+++ src/main/java/frc/robot/commands/auto/MoveXInchesBackwards.java
@@ -23,10 +23,7 @@ public class MoveXInchesBackwards extends CommandBase {
      * This method runs whenever the command is scheduled
      */
     public void initialize() {
-        driveSystem.leftMotorLeader.setOpenLoopRampRate(1);
-        driveSystem.leftMotorFollower.setOpenLoopRampRate(1);
-        driveSystem.rightMotorLeader.setOpenLoopRampRate(1);
-        driveSystem.rightMotorFollower.setOpenLoopRampRate(1);
+        driveSystem.setRampRate(0.5);
         startingPos = new double[2];
         startingPos[0] = fieldPositioningSystem.position[0];
         startingPos[1] = fieldPositioningSystem.position[1];
@@ -61,11 +58,9 @@ public class MoveXInchesBackwards extends CommandBase {
      * This method runs when the command terminates
      */
     public void end(boolean interrupted) {
+        driveSystem.setRampRate(1.5);
         driveSystem.tankDrive(0, 0);
-        driveSystem.leftMotorLeader.setOpenLoopRampRate(0);
-        driveSystem.leftMotorFollower.setOpenLoopRampRate(0);
-        driveSystem.rightMotorLeader.setOpenLoopRampRate(0);
-        driveSystem.rightMotorFollower.setOpenLoopRampRate(0);
+        driveSystem.setRampRate(0);
     }
 
     /**
